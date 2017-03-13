b58d8560401dcc07553b260ab757c7bef80cb285
Ruby Lim <ruby.b.lim@gmail.com>
2017-03-12 18:45:11 -0700

    Assignment 11 Dom Scripting: Album View completed

--- scripts/album.js
+++ scripts/album.js
@@ -28,6 +28,28 @@ var albumMarconi = {
     ]
 };
 
+var albumBeyonce = {
+    title: 'Lemonade',
+    artist: 'Beyonce',
+    label: 'Columbia',
+    year: '2016',
+    albumArtUrl: 'assets/images/album_covers/02.png',
+    songs: [
+        { title: 'Pray You Catch Me', duration: '3:15' },
+        { title: 'Hold up', duration: '3:41' },
+        { title: "Don't Hurt Yourself", duration: '3:53' },
+        { title: 'Sorry', duration: '3:52' },
+        { title: '6 Inch', duration: '4:20' },
+        { title: 'Daddy Lessons', duration: '4:47' },
+        { title: 'Love Drought', duration: '3:57' },
+        { title: 'Sandcastles', duration: '3:02' },
+        { title: 'Forward', duration: '1:19' },
+        { title: 'Freedom', duration: '4:49' },
+        { title: 'All Night', duration: '5:21' },
+        { title: 'Formation', duration: '3:26' }
+    ]
+};
+
 var createSongRow = function(songNumber, songName, songLength) {
     var template = 
         '<tr class="album-view-song-item">'
@@ -40,13 +62,13 @@ var createSongRow = function(songNumber, songName, songLength) {
     return template;
 };
 
-var setCurrentAlbum = function(album) {
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-    
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
+var setCurrentAlbum = function(album) {   
     albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
@@ -59,6 +81,18 @@ var setCurrentAlbum = function(album) {
     }
 };
 
+
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
+    
+    var collection = [albumPicasso, albumMarconi, albumBeyonce];
+    
+    var index = 1;
+    window.addEventListener('click', function(event) {
+        setCurrentAlbum(collection[index]);
+        index++;
+        if (index == collection.length) {
+            index = 0;
+        } 
+    });
 };
