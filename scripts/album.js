b0c64e1fcf1b8f872af9789707337295d4176705
Ruby Lim <ruby.b.lim@gmail.com>
2017-03-24 15:15:55 -0700

    Assignment 20 jQuery: Buzz Library completed

--- scripts/album.js
+++ scripts/album.js
@@ -165,6 +165,21 @@ var getSongNumberCell = function (number) {
     return $('.song-item-number[data-song-number="' + number + '"]');
 }; 
 
+var togglePlayFromPlayerBar = function () {
+    var $currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
+    if (currentSoundFile == null) {
+        
+    } else if (currentSoundFile.isPaused()) {
+        $currentlyPlayingCell.html(pauseButtonTemplate);
+        $(this).html(playerBarPauseButton);
+        currentSoundFile.play();
+    } else {
+        $(this).html(playerBarPlayButton);
+        $currentlyPlayingCell.html(playButtonTemplate);
+        currentSoundFile.pause();
+    }
+};
+
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 var playerBarPlayButton = '<span class="ion-play"></span>';
@@ -178,10 +193,11 @@ var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
+var $controlPlayPause = $('.main-controls .play-pause')
 
 $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
     $previousButton.click(previousSong);
     $nextButton.click(nextSong);
-
+    $controlPlayPause.click(togglePlayFromPlayerBar);
 });
